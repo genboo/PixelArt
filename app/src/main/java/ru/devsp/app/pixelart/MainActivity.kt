6156bc61b211e65799074673a82554b06693587d
genboo <gen.masj@gmail.com>
2018-07-26 13:09:57 +0300

    INIT

--- app/src/main/java/ru/devsp/app/pixelart/MainActivity.kt
+++ app/src/main/java/ru/devsp/app/pixelart/MainActivity.kt
@@ -13,9 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        fullscreenContent.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
 
     }
 
---
+++ app/src/main/java/ru/devsp/app/pixelart/view/components/Desktop.kt
@@ -0,0 +1,9 @@
+package ru.devsp.app.pixelart.view.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class Desktop constructor(context: Context, attrs: AttributeSet? = null) : View(context, attrs){
+
+}
